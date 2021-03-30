652d9ca753d1b432b55e88e5535019650d1337db
Rafael Barbosa <rafael.1940.b@gmail.com>
2021-03-30 14:56:26 -0300

    Adiciona codigo para passar no teste de numeros basicos

--- src/romano.cpp
+++ src/romano.cpp
@@ -2,6 +2,25 @@
 
 #include "../include/romano.h"
 
+int get_basic_value(char const roman_char) {
+    if (roman_char == 'I')
+        return 1;
+    if (roman_char == 'V')
+        return 5;
+    if (roman_char == 'X')
+        return 10;
+    if (roman_char == 'L')
+        return 50;
+    if (roman_char == 'C')
+        return 100;
+    if (roman_char == 'D')
+        return 500;
+    if (roman_char == 'M')
+        return 1000;
+
+    return -1;
+}
+
 int roman_to_decimal(char const *roman_number) {
-    return 0;
+    return get_basic_value(roman_number[0]);
 }
