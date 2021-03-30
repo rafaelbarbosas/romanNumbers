64fa6ba15e6f0a9f6a40b711b86dbc04c873c4f5
Rafael Barbosa <rafael.1940.b@gmail.com>
2021-03-30 15:29:59 -0300

    Adiciona teste para numeros romanos invalidos

--- src/testa_romano.cpp
+++ src/testa_romano.cpp
@@ -7,7 +7,7 @@
 
 using std::cout;
 
-TEST_CASE("Testing basic numbers", "[catch]") {
+TEST_CASE("Testing basic numbers", "[basic]") {
     REQUIRE(roman_to_decimal("I") == 1);
     REQUIRE(roman_to_decimal("V") == 5);
     REQUIRE(roman_to_decimal("X") == 10);
@@ -15,4 +15,13 @@ TEST_CASE("Testing basic numbers", "[catch]") {
     REQUIRE(roman_to_decimal("C") == 100);
     REQUIRE(roman_to_decimal("D") == 500);
     REQUIRE(roman_to_decimal("M") == 1000);
+}
+
+TEST_CASE("Testing invalid numbers", "[ivalid]") {
+    REQUIRE(roman_to_decimal("IIII") == -1);
+    REQUIRE(roman_to_decimal("XXXX") == -1);
+    REQUIRE(roman_to_decimal("VX") == -1);
+    REQUIRE(roman_to_decimal("IIX") == -1);
+    REQUIRE(roman_to_decimal("LM") == -1);
+    REQUIRE(roman_to_decimal("IM") == -1);
 }
