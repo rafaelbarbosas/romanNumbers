dfd792ebccf76553e8982e41332eba2e28742a0c
Rafael Barbosa <rafael.1940.b@gmail.com>
2021-03-30 14:51:09 -0300

    Adiciona primeiro teste -- numeros base

--- src/testa_romano.cpp
+++ src/testa_romano.cpp
@@ -7,6 +7,12 @@
 
 using std::cout;
 
-TEST_CASE("Testing catch framework", "[catch]") {
+TEST_CASE("Testing basic numbers", "[catch]") {
     REQUIRE(roman_to_decimal("I") == 1);
+    REQUIRE(roman_to_decimal("V") == 5);
+    REQUIRE(roman_to_decimal("X") == 10);
+    REQUIRE(roman_to_decimal("L") == 50);
+    REQUIRE(roman_to_decimal("C") == 100);
+    REQUIRE(roman_to_decimal("D") == 500);
+    REQUIRE(roman_to_decimal("M") == 1000);
 }
